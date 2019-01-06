983f0dbf1a37063871b2e385f481bff439047f59
Shradheya Thakre <tshradheya@gmail.com>
2019-01-06 10:50:15 -0500

    React router set up

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './store';
 import App from './components/App';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
     document.getElementById('root')
 );
